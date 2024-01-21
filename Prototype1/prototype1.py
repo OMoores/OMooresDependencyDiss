e5935f59591020def5fed19fb85c57339201b468
FieldMarshalGaig <mooresoscar1@gmail.com>
2024-01-21 08:51:11 +0000

    Added removeDuplicate and removeDuplicates functions
    These functions remove duplicate items from list, preparing it for use by pySAT

--- Prototype1/prototype1.py
+++ Prototype1/prototype1.py
@@ -41,7 +41,7 @@ class Material():
 
     def parseDependencies(self, xmlDependencies):
         """
-                Takes the dependencies section of an XML file and turns it into a list of dependencies
+            Takes the dependencies section of an XML file and turns it into a list of dependencies
         """
 
         dependencyList = []
@@ -51,7 +51,72 @@ class Material():
 
         return dependencyList
 
+    def getDependencyClause(self):
+        """
+            Looks at the dependencies for the object and returns a dependency clause that can be used with pysat
+        """
         
+        clause = []
+
+        for dependency in self.dependencies:
+            clause.append(materialDict[dependency])
+
+        return clause
+    
+    def getDependencies(self, previousClauses = []):
+        """
+            Gets all dependencies from this material, including dependencies of dependencies
+        """
+
+        clauses = self.getDependencyClause()
+
+        # Add two clauses together
+        clauses.append(previousClauses)
+
+        # Remove any duplicate materials
+
+    def removeDuplicates(list):
+        """
+            Removes duplicate items from a list
+        """
+
+        returnList = list
+        for item in list:
+            returnList = Material.removeDuplicate(list, item)
+
+        return returnList
+
+        
+
+        return list
+
+    def removeDuplicate(list, duplicate):
+        """
+            Checks list for item and removes all but the first instance of this item
+        """
+
+        # Has the first occurence of the item been found
+        found = False
+
+        # Looks through every item in list and checks if it is the same as the duplicate
+        index = 0
+        while index < len(list):
+            if list[index] == duplicate:
+
+                if found == True:
+                    list.pop(index)
+                    continue
+                found = True
+            index +=1
+
+        return list
+    
+    
+
+
+
+
+    
 
 # Get XML file
 tree = etree.parse("./TestMaterial/NatInspired/GenProg.xml")
@@ -69,6 +134,18 @@ materialDict = {}
 for mat in materialArray:
     materialDict[mat.name] = mat.materialNum
 
+clauses = []
+
+for mat in materialArray:
+    clauses.append(mat.getDependencyClause())
+
+clauses = list(filter(lambda clause: (len(clause) > 0), clauses))
+print(clauses)
 
 g = Glucose3()
+
+list = [1,2,2,2,1,1,4,5,6,3,4,5,4]
+print(Material.removeDuplicates(list))
+
+
 
