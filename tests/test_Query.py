71a83f222168fd16e1f84ec78b34b3ce28ce9260
FieldMarshalGaig <mooresoscar1@gmail.com>
2024-02-12 17:15:19 +0000

    Created some new tests

--- tests/test_Query.py
+++ tests/test_Query.py
@@ -77,6 +77,19 @@ class TestQuery(unittest.TestCase):
         self.mathsAndLectureClause.addDependencyLevel("*")
         self.mathsAndLectureClause.addVariable([""])
 
+
+        """ Creating queries for use in tests """
+        self.queryForAllMats = Query() # Queries for all materials
+        self.queryForAllMats.addClause(self.allMatClause)
+
+        self.queryForRecommendsOrLecture = Query() 
+        self.queryForRecommendsOrLecture.addClauses([self.allRecommendsClause,self.allLecturesClause])
+
+        self.queryForRecommendsORMathsANDLecture = Query()
+        self.queryForRecommendsORMathsANDLecture.addClauses([self.allRecommendsClause,self.mathsAndLectureClause])
+
+        self.queryForMathsANDLectureORNOTLecture = Query()
+        self.queryForMathsANDLectureORNOTLecture.addClauses([self.mathsAndLectureClause,self.allNotLecturesClause])
         
 
 
@@ -169,26 +182,79 @@ class TestQuery(unittest.TestCase):
 
     def test_isQueryValid(self):
         """
-            Description: Test the function isMaterialValid, this function takes a material and its dependency level along with a query and returns if the material and dependency is valid for this query
+            Description: Test the function isQueryValid, this function takes a material and its dependency level along with a query and returns if the material and dependency is valid for this query
 
             dependency -> [material : Material, dependency level : str]
 
             Expected results: The function returns true for valid materials and false for invalid ones
         """
 
+        # Test a material to check it is any material - True
+        test1 = Query.isQueryValid([self.mathsLectureMat,"Requires"],self.queryForAllMats)
+        self.assertTrue(test1)
+
+        # Test if a Recommended workshop is recommended OR a lecture - True
+        test2 = Query.isQueryValid([self.mathsWorkshopMat,"Recommends"],self.queryForRecommendsOrLecture)
+        self.assertTrue(test2)
+
+        # Test if a Required workshop is recommended OR a lecture - False
+        test3 = Query.isQueryValid([self.mathsWorkshopMat,"Recommends"],self.queryForRecommendsOrLecture)
+        self.assertFalse(test3)
+
+        # Test if a Required lecture is recommended or a lecture - True
+        test4 = Query.isQueryValid([self.mathsLectureMat,"Requires"],self.queryForRecommendsOrLecture)
+        self.assertTrue(test4)
+        
+        # Test if a required maths lecture is recommended OR maths AND a lecture - True
+        test5 = Query.isQueryValid([self.mathsLectureMat,"Requires"],self.queryForRecommendsORMathsANDLecture)
+        self.assertTrue(test5)
+
+        # Test if a required maths workshop is recommended OR maths AND a lecture - False
+        test6 = Query.isQueryValid([self.mathsWorkshopMat,"Requires"],self.queryForRecommendsORMathsANDLecture)
+        self.assertFalse(test6)
+
+        # Test if a required maths lecture is maths AND a lecture OR not a lecture - True
+        test7 = Query.isQueryValid([self.mathsLectureMat,"Requires"],self.queryForMathsANDLectureORNOTLecture)
+        self.assertTrue(test7)
+
+        # Test if a physics lecture is maths AND a lecture OR not a lecture - False
+        test8 = Query.isQueryValid([self.physicsLectureMat,"Requires"],self.queryForMathsANDLectureORNOTLecture)
+        self.assertFalse(test8)
+
+        # Test if a physics workshop is maths AND a lecture or not a lecture - True
+        test9 = Query.isQueryValid([self.physicsWorkshopMat,"Requires"],self.queryForMathsANDLectureORNOTLecture)
+        self.assertTrue(test9)
+       
+
+
 
 
     def test_isDependencyValid(self):
         """
-            Description: Test the isDependencyValid function, tests a dependency against a clause to see if the dependency is in the clause
+            Description: Test the isDependencyValid function, tests a dependency level against a clause to see if the dependency is in the clause
 
             Expected results: The function returns true when the dependency is present in the clause and false when it is not
         """
 
+        # Testing if a recommended material is recommended - True
+        test1 = Query.isDependencyValid("Recommends", self.allRecommendsClause)
+        self.assertTrue(test1)
 
+        # Testing if a required material is required - False
+        test2 = Query.isDependencyValid("Requires",self.allRecommendsClause)
+        self.assertFalse(test2)
 
+        # Testing if a required material is NOT required - False
+        test3 = Query.isDependencyValid("Requires", self.allNotRequiresClause)
+        self.assertFalse(test3)
 
+        # Testing if a recommended material is NOT required - True
+        test4 = Query.isDependencyValid("Recommends", self.allNotRequiresClause)
+        self.assertTrue(test4)
 
+        # Testing if a required material is any dependency - True
+        test5 = Query.isDependencyValid("Requires", self.allMatClause)
+        self.assertTrue(test5)
 
 
     def test_findValidMaterials(self):
